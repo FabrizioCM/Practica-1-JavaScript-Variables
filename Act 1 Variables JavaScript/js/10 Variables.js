ca88e08f4c88af28557320d625aca87b6547e7ca
FabrizioCM <142865932+FabrizioCM@users.noreply.github.com>
2023-09-19 10:09:21 -0600

    Add files via upload

---
+++ Act 1 Variables JavaScript/js/10 Variables.js
@@ -0,0 +1,63 @@
+alert ('Conectado')
+//*Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+
+// 10 variables booleanas
+let esVerdad = true;
+let esMentira = false;
+let esUno = true;
+let esCero= false;
+let esCorrecto = false;
+let esIncorrecto = false;
+let esAbsoluto = true;
+let esFinito = true;
+let esInfinito = true;
+let estaAlineado = true;
+
+// 10 variables String
+let nombreUno ="Xbox";
+let nombreDos ="PlaySation";
+let nombreTres ="Nintendo";
+let nombreCuatro ="Iphone";
+let nombreCinco ="juegosfera";
+let nombreSeis ="Bimo";
+let nombreSiete ="PSP";
+let nombreOcho ="GameBoy";
+let nombreNueve ="Atari";
+let nombreDiez ="GameCube";
+
+// 10 variables numericas
+let primerNumero = 1;
+let segundoNumero = 2;
+let tercerNumero = 3;
+let cuartoNumero = 4;
+let quintoNumero = 5;
+let sextoNumero = 6;
+let septimoNumero = 7;
+let octavoNumero = 8;
+let novenoNumero = 9;
+let decimoNumero = 10;
+
+
+//  Declarar variables numericas y realizar las operaciones aritmeticas basicas
+
+let suma = primerNumero + segundoNumero;
+let resta = novenoNumero - sextoNumero;
+let multiplicacion = tercerNumero * quintoNumero;
+let division = decimoNumero / cuartoNumero; 
+
+// Declarar variables que concatenen cadenas de texto
+
+let unionUno =  nombreUno + nombreCinco;
+let unionDos =  nombreCuatro + nombreDiez; 
+let unionTres = nombreCinco + nombreSiete;
+let unionCuatro = nombreTres + nombreDos;
+let unionCinco = nombreSeis + nombreNueve;
+console.log ("unionUno, unionDos, unionTres, UnionCuatro, UnionCinco");
+
+//  Declarar 5 variables y convertirlas a otro tipo de dato
+
+let nombreUnoBoolean = Boolean (nombreUno);
+let nombreDosString = String (nombreDos);
+let nombreTresNumber = Number (nombreTres);
+let nombreCuatroBoolean = Boolean (nombreCuatro);
+let nombreCincoNumber = Number (nombreCinco);
